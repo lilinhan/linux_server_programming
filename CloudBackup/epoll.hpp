bdc056878cd16dac9898db88d9eedc3a36d8dcef
lilinhan <lilinhan1303@gmail.com>
2015-07-27 21:57:47 +0800

    little

--- CloudBackup/epoll.hpp
+++ CloudBackup/epoll.hpp
@@ -20,10 +20,15 @@
 #include<fcntl.h>
 #include<sys/epoll.h>
 #include<pthread.h>
+#include<list>
+#include<queue>
+#include<mutex>
 
 #define MAX_EVENT_NUMBER 1024
 #define TCP_BUFFER_SIZE 512
 #define LISTEN_NUMBER 1024
+
+const int MaxTaxkCount = 1000;
 
 class Epoll{
     public:
@@ -91,7 +96,158 @@ class Epoll{
 
 };
 
-class ThreadPool{
 
+template<typename T>
+class SyncQueue{
+    public:
+        SyncQueue(int m_maxSize) : m_maxSize(m_maxSize) , m_stop(false) {}
+        ~SyncQueue(){}
+
+        void Put(const T &x ) {
+            Add(x);
+        }
+
+        void Put( T && x)  {
+            Add(std::forward<T>(x));
+        }
+
+        void Take(std::list<T> & list) {
+            std::unique_lock<std::mutex> locker(m_mutex);
+            m_notEmpty.wait( locker , [this]{return m_stop || NotEmpty();});
+            if( m_stop ) {
+                return;
+            }
+            list = std::move(m_queue);
+            m_notFull.notify_one();
+        }
+
+        void Take( T & t ) {
+            std::unique_lock(std::mutex) locker(m_mutex);
+            m_notEmpty.wait( locker , [this]{ return m_stop || NotEmpty();});
+            if(m_stop)  {
+                return;
+            }
+            t = m_queue.front();
+            m_queue.pop_front();
+            m_notFull.notify_one();
+        }
+
+        void Stop()  {
+            {
+                std::lock_guard<std::mutex> locker(m_mutex);
+                m_stop = true;
+            }
+            m_notFull.notify_all();
+            m_notEmpty.notify_all();
+        }
+
+        bool Empty(){
+            std::lock_guard<std::mutex> locker(m_mutex);
+            return m_queue.size();
+        }
+
+        bool Full()  {
+            std::lock_guard<std::mutex> locker(m_mutex);
+            return m_queue.size() == m_maxSize;
+        }
+
+        size_t Size() {
+            std::lock_guard<std::mutex> locker(m_mutex);
+            return m_queue();
+        }
+
+        int Count()  {
+            return m_queue.size();
+        }
+
+    private:
+        bool NotFull()  {
+            bool full = m_queue.size() >= m_maxSize;
+            if( full )  {
+                std::cout << " The Syncqueue is full ..." << std::endl;
+            }
+            return !full;
+        }
+
+        bool NotEmpty()  {
+            bool empty = m_queue.empty();
+            if( empty )  {
+                std::cout << "The Syncqueue is Empty..." << std::endl;
+            }
+            return !empty;
+        }
+
+        template<typename F>
+        void Add( F && x ) {
+            std::unique_lock<std::mutex> locker(m_mutex);
+            m_notFull.wait(locker,[this]{return m_stop || NotFull();});
+            if( m_stop )  {
+                return;
+            }
+            m_queue.push_back(std::forward<F>(x));
+            m_notEmpty.notify_one();
+        }
+
+        std::list<T> m_queue;
+        std::mutex m_mutex;
+        std::condition_variable m_notEmpty;
+        std::condition_variable m_notFull;
+        int m_maxSize;
+        bool m_stop;
+};
+
+class ThreadPool : SyncQueue{
+    public:
+        using Task = std::function<void()>;
+        ThreadPool(int numThreads = std::thread::hardware_concurrency()):m_queue(MaxTaxkCount)  {
+            Start(numThreads);
+        }
+
+        ~ThreadPool(void) {
+            Stop();
+        }
+
+        void AddTask(const Task & t) {
+            m_queue.Put(t);
+        }
+    private:
+        void Start(int numThreads)  {
+            m_running = true;
+            for(int i = 0 ; i < numThreads ; ++i ) {
+                m_ThreadGroup.push_back(std::make_shared<std::thread>(&ThreadPool::RunInThread , this));
+
+            }
+        }
+
+        void RunInThread() {
+            while(m_running)  {
+                std::list<Task> list;
+                m_queue.Take(list);
+
+                for( auto & Task : list )  {
+                    if(!m_running)  {
+                        return;
+                    }
+                    Task();
+                }
+            }
+        }
+
+        void StopThreadGroup()  {
+            m_queue.Stop();
+            m_running = false;
+
+            for( auto thread : m_running )  {
+                if(thread)  {
+                    thread->join();
+                }
+                m_ThreadGroup.clear();
+            }
+        }
+
+        std::list<std::shared_ptr<std::thread>> m_ThreadGroup;
+        SyncQueue<Task> m_queue;
+        std::atomic_bool m_running;
+        std::once_flag m_flag;
 };
 
